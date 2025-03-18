aea2b612c3493a40c1c43f6f2421a802ef88edfb
이태환 <sheena1131@kakao.com>
2025-03-18 13:41:24 +0900

    Revert "[고승연] 오류1"
    This reverts commit 923099fb251d65ec6948e635b86f662bae02a36d.

--- Assets/02. Script/Lobby/LobbyManager.cs
+++ Assets/02. Script/Lobby/LobbyManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
@@ -10,25 +11,44 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 {
     private string gameVersion = "1";
 
+    public Button CreateButton;
     public Button joinButton;
+    public GameObject CreateUI;
+    public GameObject JoinUI;
+
+    public TMP_InputField RoomNameInput;
+    public TMP_InputField NickNameInputCreate;
+    public TMP_InputField NickNameInputJoin;
+
+    public Transform roomListContainer; 
+    public GameObject roomButtonPrefab;
+
+    private Dictionary<string, GameObject> roomButtonDict = new Dictionary<string, GameObject>();
+
 
     private void Start()
     {
         PhotonNetwork.GameVersion = gameVersion;
         PhotonNetwork.ConnectUsingSettings();
 
+        CreateUI.SetActive(false);
+        JoinUI.SetActive(false);
+        CreateButton.interactable = false;
         joinButton.interactable = false;
     }
     public override void OnConnectedToMaster()
     {
+        PhotonNetwork.JoinLobby();
+        CreateButton.interactable = true;
         joinButton.interactable = true;
     }
     public override void OnDisconnected(DisconnectCause cause)
     {
+        CreateButton.interactable = false;
         joinButton.interactable = false;
         PhotonNetwork.ConnectUsingSettings();
     }
-    public void Connect1()
+    public void Connect()
     {
         PhotonNetwork.SendRate = 30;
         PhotonNetwork.SerializationRate = 30;
@@ -37,7 +57,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         if (PhotonNetwork.IsConnected)
         {
 
-            RoomOptions roomOptions = new RoomOptions { MaxPlayers = 20 };
+            RoomOptions roomOptions = new RoomOptions { MaxPlayers = 2 };
             PhotonNetwork.JoinOrCreateRoom("Server1", roomOptions, TypedLobby.Default);
         }
         else
@@ -49,7 +69,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     {
         base.OnJoinedRoom();
         Debug.Log("Joined Room");
-
+        StartLoading();
 
     }
     public void StartLoading()
@@ -57,6 +77,93 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
         PhotonNetwork.LoadLevel("TH");
 
+    }
 
+    public void RoomCreateUI()
+    {
+        CreateUI.SetActive(true);
+    }
+    public void RoomJoinUI()
+    {
+        JoinUI.SetActive(true);
+    }
+    public void CreateRoom()
+    {
+        Debug.Log("방 생성 버튼 클릭.");
+
+        string roomName = RoomNameInput.text;
+        string nickName = NickNameInputCreate.text;
+
+        if (string.IsNullOrEmpty(roomName))
+        {
+            Debug.Log("방 이름이 비어있습니다.");
+            return;
+        }
+        if (string.IsNullOrEmpty(nickName))
+        {
+            Debug.Log("닉네임이 비어있습니다.");
+            return;
+        }
+
+        PhotonNetwork.NickName = nickName;
+        RoomOptions roomOptions = new RoomOptions { MaxPlayers = 20 };
+        PhotonNetwork.CreateRoom(roomName, roomOptions);
+    }
+   
+    public void JoinRoom(string roomName)
+    {
+        string nickName = NickNameInputJoin.text;
+        if (string.IsNullOrEmpty(nickName))
+        {
+            Debug.Log("닉네임이 비어있습니다.");
+            return;
+        }
+
+        PhotonNetwork.NickName = nickName;
+        PhotonNetwork.JoinRoom(roomName);
+    }
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo room in roomList)
+        {
+            if (room.RemovedFromList)
+            {
+                if (roomButtonDict.ContainsKey(room.Name))
+                {
+                    Destroy(roomButtonDict[room.Name]);
+                    roomButtonDict.Remove(room.Name);
+                }
+            }
+            else
+            {
+                if (!roomButtonDict.ContainsKey(room.Name))
+                {
+                    GameObject roomButton = Instantiate(roomButtonPrefab, roomListContainer);
+                    TMP_Text buttonText = roomButton.GetComponentInChildren<TMP_Text>();
+                    if (buttonText != null)
+                    {
+                        buttonText.text = $"{room.Name} ({room.PlayerCount}/{room.MaxPlayers})";
+                    }
+
+                    Button button = roomButton.GetComponent<Button>();
+                    if (button != null)
+                    {
+                        button.onClick.AddListener(() => { JoinRoom(room.Name); });
+                    }
+                    roomButtonDict.Add(room.Name, roomButton);
+                }
+                else
+                {
+                    GameObject roomButton = roomButtonDict[room.Name];
+                    TMP_Text buttonText = roomButton.GetComponentInChildren<TMP_Text>();
+                    if (buttonText != null)
+                    {
+                        buttonText.text = $"{room.Name} ({room.PlayerCount}/{room.MaxPlayers})";
+                    }
+                }
+            }
+        }
     }
 }
+
+
